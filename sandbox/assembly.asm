2b73f844f433bc132fbc53dd7e6ee4aec310305b
Arrest0ron <130374519+Arrest0ron@users.noreply.github.com>
2024-09-09 13:56:06 +0000

    2.8 done

---
+++ sandbox/assembly.asm
@@ -0,0 +1,14 @@
+format ELF
+public _start
+msg db "Mushkarin", 0xA, "Dmitri", 0xA,"Evgenievich", 0xA, 0
+
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, 29
+    int 0x80
+
+    mov eax, 1
+    mov ebx, 0
+    int 0x80

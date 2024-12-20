3db25c40a13340c538e414b472141f5e5fd4987e
Arrest0ron <130374519+Arrest0ron@users.noreply.github.com>
2024-12-20 19:17:40 +0300

    four done

--- lab7/task2.asm
+++ lab7/task2.asm
@@ -8,28 +8,46 @@ include 'func.asm'
 
 section '.data' writable
   randomfile db "/dev/random", 0
-   msg_1 db "First process",0
-   msg_2 db "Second process",0
-   msg_3 db "Third process",0
-   N dq 629
+   msg_1 db "Кратно пяти: ",0
+   msg_2 db "Третье после максимального: ",0
+   msg_3 db "Количество чисел, сумма цифр которых кратна 3: ",0
+   msg_4 db "Самая редкая цифра: ", 0
+   N dq 600
    BYTE_SIZE dq 0
   num_1 dw 0
   num_2 dw 0
   num_3 dw 0
 	num_4 dw 0
+  some_number dw 22333
+    rarest_one dw 0
+  
+  rarest_amount dq 0
 	
 section '.bss' writable
 	
 	buffer rb 100
+  buf2 rb 120
+  digits_array rq 10
+  digit rb 2
 	address rq 1
   number rw 1
+
+  
 
 section '.text' executable
 	
 _start:
-    mov rsi, msg_1
-    call print_str
-    call new_line
+    ; mov rsi, msg_1
+    ; call print_str
+
+
+    ; call new_line
+    ; mov rax, [digits_array+3*8]
+    ; call number_str
+    ; call print_str
+    ; call exit
+    
+    ; call new_linem
 
     mov rax, [N]
     mov rbx, 2
@@ -57,33 +75,136 @@ _start:
    syscall 
    mov r8, rax
     call filler
+    call printer
 
     ;;Делаем fork процесса
     
     mov rax, 57
     syscall
     cmp rax, 0
-    je fork_process1
+    je divisible_by_five
 
     mov rax, 57
     syscall
     cmp rax, 0
-    je fork_process2
-
+    je  sum_divisible_by_three 
     mov rax, 57
     syscall
     cmp rax, 0
-    je third
-    
+    je third_after_max
+        mov rax, 57
+    syscall
+    cmp rax, 0
+    je rarest_digit
     ;; выполняем системный вызов munmap, освобождая память
     mov rdi, [address]
     mov rsi, 1
     mov rax, 11
     syscall
     call exit
+
+; rdi - source string array
+add_digits_to_arrays:
+  push rax
+  push rbx
+  push rcx
+  push rdx
+  xor rcx, rcx
+  xor rcx, rcx
+  .cloop:
+  xor rax, rax
+    mov BYTE al, [rdi+rcx]
+    inc rcx
+    cmp al, 0
+    je .end
+    sub al, 48
+    ; call exit
+
+    inc [digits_array+rax*8]
+    
+    jmp .cloop
+
+  .end:
+  pop rdx
+  pop rcx
+  pop rbx
+  pop rax
+  ret
+ 
+minimal_array_index:
+push rax
+ push rbx
+ push rcx
+ push rdx
+   xor rcx,rcx
+   xor r8, r8
+  mov rsi, buf2
+   .mlp: 
+  ;  mov rax, [rar]
+      ;  call number_/str
+    ; call print_str
+    mov rax, [digits_array+rcx*8]
+
+    cmp rax, [rarest_amount]
+    jl .next
+    mov [rarest_one], cx
+    mov [rarest_amount], rax
+    ; call new_line
+    .next:
+    inc rcx
+    cmp rcx, 10
+    
+   jne .mlp 
+  ;  call new_line
+  ;  call new_line
+   pop rdx
+   pop rcx
+   pop rbx 
+   pop rax
+
+   ret
+
+rarest_digit:
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    xor rcx, rcx
+    mov rsi, buffer
+    xor r8, r8
+    .mloop:
+      push rcx
+      xor rbx, rbx
+      xor rax, rax
+      mov rbx, [address+rcx*2]
+      mov ax, bx
+      mov rsi, buffer
+      call number_str
+      mov rdi, rsi
+      call add_digits_to_arrays
+      
+      inc rcx
+      cmp rcx, [N]
+      jl .mloop
+      mov rsi, msg_4
+      call print_str
+      call minimal_array_index
+  
+    mov ax, [rarest_one]
+    call number_str
+    call print_str
+
+    call new_line
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    call exit
+
+
   
 
-  fork_process1:
+  sum_divisible_by_three:
     push rax
     push rbx
     push rcx
@@ -102,14 +223,16 @@ _start:
       div rbx
       cmp rdx, 0
       jne .end
-      inc r8
+      inc r9
 
       .end:
       pop rcx
       inc rcx
       cmp rcx, [N]
       jl .mloop
-    mov rax, r8
+      mov rsi, msg_3
+      call print_str
+    mov rax, r9
     call number_str
     call print_str
     call new_line
@@ -119,7 +242,7 @@ _start:
     pop rax
     call exit
 
-  fork_process2:
+  divisible_by_five:
     push rax
     push rbx
     push rcx
@@ -145,6 +268,8 @@ _start:
       inc rcx
       cmp rcx, [N]
       jl .mloop
+      mov rsi, msg_1
+      call print_str
     mov rax, r8
     call number_str
     call print_str
@@ -155,7 +280,7 @@ _start:
     pop rax
     call exit
 
-third:
+third_after_max:
   push rax
   push rbx
   push rcx
@@ -208,12 +333,13 @@ third:
     inc rcx
     cmp rcx, [N]
     jl .mloop
+    mov rsi, msg_2
+    call print_str
     xor rax, rax
     mov ax, [num_4]
-    call new_line
+    
     call number_str
     call print_str
-    call new_line
     call new_line
   pop rdx
   pop rcx

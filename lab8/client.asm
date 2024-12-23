beb01e7464dd11e40df234c9b543055ada901d49
Arrest0ron <130374519+Arrest0ron@users.noreply.github.com>
2024-12-24 00:30:42 +0300

    general logic done

--- lab8/client.asm
+++ lab8/client.asm
@@ -1,26 +1,44 @@
 ;;client_2.asm
-
 format elf64
 public _start
+
 include 'func.asm'
 
 section '.data' writeable
-  
+take_msg db 'Ваша карта: %d, сумма %d', 0xA,0
   msg_1 db 'Error bind', 0xa, 0
   msg_2 db 'Successfull bind', 0xa, 0
   msg_3 db 'Successful connect', 0xa, 0
   msg_4 db 'Error connect', 0xa, 0
-
+  msg_enter db 'Вы присоединились к игре. Отправьте T, чтобы взять карту, или S, чтобы остановиться.', 0xa, 0
+  number dq 0
+  msg_lost db 'Переполнение! Вы проиграли.', 0xA, 0
+  reading_msg db '_r_', 0
+  
+  ender db 'Ending coonection', 0xA, 0
+  random_resp db 'Ваше число - %d', 0xA, 0
+  stop_msg db 'Вы остановились на счете %d', 0xA,0
+  RANDOM dq 0
+  quit db 'Q',0
+  MAX dq 12
+  MIN dq 3
+  f  db "/dev/urandom",0
+  sm1 dw 0, -1, 4096 
+  sm2 dw 0,  1, 4096   
+  ids dq 0
+  balance dq 0
 section '.bss' writable
-	
+	random_desc rq 1
   buffer1 rb 100
   buffer2 rb 100
+  server rq 1
+
   
 
 struc sockaddr_client
 {
   .sin_family dw 2         ; AF_INET
-  .sin_port dw 0x4d9     ; port 55556
+  .sin_port dw 0    ; port 55556
   .sin_addr dd 0           ; localhost
   .sin_zero_1 dd 0
   .sin_zero_2 dd 0
@@ -43,7 +61,60 @@ addrlen_server = $ - addrstr_server
 
 section '.text' executable
 	
+extrn printf
 _start:
+
+   mov rdi, f
+   mov rax, 2 
+   mov rsi, 0o
+   syscall 
+   mov [random_desc], rax
+
+
+  ;  mov rax, 0 ;
+  ;   mov rdi, [random_desc]
+  ;   mov rsi, number
+  ;   mov rdx, 1
+  ;   syscall
+  
+    ; mov rax, [number]
+    ; xor rdx, rdx
+    ; mov rcx, 100
+    ; div rcx
+    ; inc rdx
+    ; add [addrstr_client+2], rdx
+    ; xor rdx,rdx
+    ; mov rax, [addrstr_client+2]
+    ; mov rsi, buffer1
+    ; call number_str
+    ; call print_str
+    ; call exit
+
+
+  ;  call take_card
+  ;   call exit
+    
+   
+  ;   call exit
+    ;;Создаем семафор
+    mov rdi, 0
+    mov rsi, 1
+    mov rdx, 438 ;;0o666
+    or rdx, 512
+    mov rax, 64
+    syscall
+    
+    mov [ids], rax
+    
+    ;;Переводим семафор в состояние готовности
+    mov rdi, [ids] ;дескриптор семафора
+    mov rsi, 0     ;индекс в массиве
+    mov rdx, 16    ;выполняемая команда
+    mov r10, 0   ;начальное значение
+    mov rax, 66
+    syscall
+    
+
 
     ;;Создаем сокет клиента
     mov rdi, 2 ;AF_INET - IP v4 
@@ -51,9 +122,13 @@ _start:
     mov rdx, 6 ;TCP
     mov rax, 41
     syscall
-    
+    cmp rax, 0
+    je _bind_error
+
+
     ;;Сохраняем дескриптор сокета клиента
     mov r9, rax
+    mov [server], rax
     
        
     ;;Связываем сокет с адресом
@@ -80,30 +155,48 @@ _start:
     
     cmp rax, 0
     jl  _connect_error
-         mov rax, 57
-     syscall
 
+    mov rax, 57
+     syscall
      cmp rax,0
      je _read
      
      mov rax, 57
      syscall
-   
      cmp rax,0
      je _write
+
+      mov rsi, msg_enter
+     call print_str
+
     .main_loop:
-     jmp .main_loop
-    
-    ;;Закрываем чтение, запись из клиентского сокета
-    mov rax, 48
-    mov rdi, r9
-    mov rsi, 2
+    mov rdi, [ids]
+    mov rsi, sm1
+    mov rdx,1
+    mov rax, 65
     syscall
+      mov rsi, ender
+      call print_str
+     call exit
+    .end:
+    mov rsi, ender
+    call print_str
+
+    mov rdi, [ids]
+    mov rsi, sm2
+    mov rdx,1
+    mov rax, 65
+    syscall
+    ; ;;Закрываем чтение, запись из клиентского сокета
+    ; mov rax, 48
+    ; mov rdi, r9
+    ; mov rsi, 2
+    ; syscall
           
-    ;;Закрываем клиентский сокет
-    mov rdi, r9
-    mov rax, 3
-    syscall
+    ; ;;Закрываем клиентский сокет
+    ; mov rdi, r9
+    ; mov rax, 3
+    ; syscall
     
     call exit
 
@@ -119,19 +212,40 @@ _connect_error:
    call exit
 
 _read:
+
+    ; mov rsi, reading_msg
+    ; call print_str
       mov rax, 0 ;номер системного вызова чтения
       mov rdi, r9 ;загружаем файловый дескриптор
       mov rsi, buffer1 ;указываем, куда помещать прочитанные данные
       mov rdx, 100 ;устанавливаем количество считываемых данных
+      push rsi
+      ; mov rsi, msg_3
+      ; call print_str
+      ; pop rsi
       syscall ;выполняем системный вызов read
-      
-      ;;Если клиент ничего не прислал, продолжаем
+      ; mov rsi, msg_3
+      ; call print_str
+      ; pop rsi
+    ;   ;;Если клиент ничего не прислал, продолжаем
+      ; mov rsi, buffer1
+      ; call number_str
+      ; call print_str
       cmp rax, 0
-      je _read     
-      call print_str
-      call new_line
+      je _read    
+      ; jne _read
       
-      ;;Очищаем буффер, чтобы он не хранил старые значения
+
+      mov rdi, buffer1
+      push rcx
+      call print_str
+      pop rcx
+      call new_line
+
+
+
+      
+    ;   ;;Очищаем буффер, чтобы он не хранил старые значения
       mov rcx, 100
       mov rax, 0
       .lab:
@@ -140,13 +254,89 @@ _read:
 jmp _read
 
 _write:
-     mov rsi, buffer2
-     call input_keyboard
     
-    ;;Отправляем сообщение на клиенту
+    mov rsi, buffer2
+    call input_keyboard
+    
+    cmp byte [buffer2], 'Q'
+    je _start.end
+
+    cmp byte [buffer2], 'T'
+    jne .next1
+    call take_card
+    jmp .next2
+    
+
+
+    .next1:
+    cmp byte [buffer2], 'S'
+    jne .next2
+    call stop_take
+    
+    ; jmp _write
+    .next2:
+    
+    ;;Отправляем сообщение на сервер
     mov rax, 1
-    mov rdi, r9
+    mov rdi, [server]
     mov rsi, buffer2
     mov rdx, 100
     syscall
+
+      ; mov rcx, 100
+      ; .lab:
+      ;   mov [buffer2+rcx], 0
+      ; loop .lab 
 jmp _write
+
+
+take_card:
+    mov rax, 0 ;
+    mov rdi, [random_desc]
+    mov rsi, number
+    mov rdx, 1
+    syscall
+    mov rax, [MAX]
+    sub rax, [MIN]
+    mov rcx, rax ;rcx=5
+    mov rax, [number] ;rax = rand
+    xor rdx, rdx
+    div rcx
+    mov rax, rdx
+    add rax, [MIN]
+    mov [RANDOM], rax
+    mov rdi, take_msg
+    mov rsi, [RANDOM]
+    add [balance], rax
+    cmp [balance], 22
+    jl .nnnext
+    mov rsi, msg_lost
+    call print_str
+    mov [balance], 0
+    jmp wh_l
+
+    .nnnext:
+    mov rdx, [balance]
+    push rax
+    call printf
+    pop rax
+    mov rax, [RANDOM]
+    wh_l:
+    mov BYTE [buffer2], '!'
+    mov BYTE [buffer2+1], al
+    mov BYTE [buffer2+2], 0
+    
+    
+    ret
+
+stop_take:
+    
+    mov rdi, stop_msg
+    mov rsi, [balance]
+    push rax
+    call printf
+    pop rax
+    mov BYTE [buffer2], '#'
+    mov BYTE [buffer2+1], 0
+    mov [balance], 0
+    ret
--- lab8/server.asm
+++ lab8/server.asm
@@ -13,13 +13,27 @@ section '.data' writeable
   msg_1 db 'Error bind', 0xa, 0
   msg_2 db 'Successfull bind', 0xa, 0
   msg_3 db 'New connection on server',0xA, 0
+  score_msg db 'Игрок %d закончил игру со счетом %d', 0xA, 0
   msg_4 db 'Successfull listen', 0xa, 0
-  ping_msg db '[pingall]', 0xA, 0
-  from_msg db '[usr%d]: '
-
+  reset_amount_msg db 'Перезапускаем игру для %d игроков', 0xA, 0
+  new_game_msg db 'Новая игра началась!', 0xA, 0
+  ping_msg db '[ping%d]', 0xA, 0
+  from_msg db '[usr%d]: ', 0xA, 0
+  other_takes_msg db '<Игрок %d взял карту %d, его счет %d>', 0xA, 0
+  other_stop_msg db '<Игрок %d остановился на счете %d>', 0xA, 0
+  other_lost_msg db '<Игрок %d проиграл со счетом %d>', 0xA, 0
+  other_connected db '<Присоединился новый игрок - usr%d>', 0xA, 0
+  left_players_msg db '<Осталось %d игроков>', 0xA, 0
+  endgame_msg db 'Игра окончена. Подсчет результатов...', 0xA,0
   automated_response db 'ping client', 0xA, 0
-  address dq 0
+  random_resp db 'Ваше число - %d', 0xA, 0
+  new_player_stats db 'Имеется %d игроков, %d - активно.',0xA,0       
+  cards_scores_players_current dq 0 ; +0-63 scores (up to 64 players) , +64 - total players
   new_status db 0
+  enders dq 0  ; +0-63 enders, +64 - current_ended
+  inp_msg db '_in_', 0xA, 0
+  message_to_all dq  0 ;+0-64 - msg
+
 
   ids dq 0
   val dq 0
@@ -32,9 +46,8 @@ section '.bss' writable
   buffer rb 100
   buffer_help rb 200
   read_buffer rb 100
-  descriptors dq 2
-  my_status dq 0
 
+  random_desc rq 1
 ;;Структура для клиента
   struc sockaddr_in
 {
@@ -44,13 +57,13 @@ section '.bss' writable
   .sin_zero_1 dd 0
   .sin_zero_2 dd 0
 }
-
   addrstr sockaddr_in 
   addrlen = $ - addrstr
-
 section '.text' executable
 	
 _start:
+
+
     ;;Создаем семафор
     mov rdi, 0
     mov rsi, 1
@@ -68,13 +81,9 @@ _start:
     mov r10, 0   ;начальное значение
     mov rax, 66
     syscall
-
-
-    
-  
       ;;Первый процесс создает разделяемую память
     mov rdi, 0    ;начальный адрес выберет сама ОС
-    mov rsi, 2    ;задаем размер области памяти
+    mov rsi, 512    ;задаем размер области памяти
     mov rdx, 0x3  ;совмещаем флаги PROT_READ | PROT_WRITE
     mov r10, 0x21  ;задаем режим MAP_ANONYMOUS|MAP_SHARED
     mov r8, -1   ;указываем файловый дескриптор null
@@ -82,8 +91,13 @@ _start:
     mov rax, 9    ;номер системного вызова mmap
     syscall
     
-    ;;Сохраняем и печатаем адрес памяти
-    mov [address], rax
+    ;;Сохраняем адрес памяти
+    mov [cards_scores_players_current], rax
+    mov [enders], rax
+    add [enders], 65
+    mov [message_to_all], rax
+    add [message_to_all], 65
+    add [message_to_all], 65
 
     ;;Создаем сокет
     mov rdi, 2 ;AF_INET - IP v4 
@@ -121,33 +135,51 @@ _start:
 
     mov rax, 57
      syscall
-   
      cmp rax,0
      je _input
     
     ;;Главный цикл ожидания подключений
     .main_loop:
-      
-      ;;
       mov rax, 43
       mov rdi, r9
       mov rsi, 0
       mov rdx, 0
       syscall
-
       ;;Сохраняем дескриптор сокета клиента
       mov r12, rax
-
       mov rsi, msg_3
       call print_str
-      mov rdi, [address]
-      inc BYTE [rdi]
-      mov rsi, buffer
-      xor rax, rax
-      mov rax ,[address]
-      call number_str
-      call print_str
+      mov rdi, [cards_scores_players_current]
+      inc byte [rdi+64]  ;players count
+      mov rdi, [enders]
+      inc BYTE [rdi+64]
 
+      mov rdi, [enders]
+      xor rax, rax
+      mov al, [rdi+64]
+      mov rdx, rax
+      mov rdi, [cards_scores_players_current]
+      mov al, [rdi+64]
+      mov rsi, rax
+      ; mov rsi, 0
+      ; mov rdx, 0
+      mov rdi, new_player_stats
+      push rsi 
+      push rcx
+      push rax
+      push rdx
+      push r9
+      ; push r10
+      push r12
+      call printf
+      pop r12
+      ; pop r10
+      pop r9
+      pop rdx
+      pop rax
+      pop rcx
+      pop rsi
+      
      ;;Делаем fork for read
    
      mov rax, 57
@@ -164,7 +196,7 @@ _start:
       
      jmp .main_loop
         ;; выполняем системный вызов munmap, освобождая память
-    mov rdi, [address]
+    mov rdi, [cards_scores_players_current]
     mov rsi, 1
     mov rax, 11
     syscall
@@ -182,12 +214,104 @@ _read:
       mov rdx, 100 ;устанавливаем количество считываемых данных
       syscall ;выполняем системный вызов read
 
-
-      
-      
       ;;Если клиент ничего не прислал, продолжаем
+ 
+
       cmp rax, 0
       je _read     
+
+      cmp BYTE [read_buffer], '!'
+      jne .next12
+
+      mov rbx, [cards_scores_players_current]
+      
+      xor rax, rax
+      mov al, [read_buffer+1]
+      add BYTE [rbx+r12], al
+
+      mov rdi, other_takes_msg
+      xor rax, rax
+      mov al, [read_buffer+1]
+      mov rsi, r12
+      mov rdx, rax
+      mov rbx, [cards_scores_players_current]
+      xor rcx, rcx
+      mov cl, BYTE [rbx+r12]
+
+      ; push rax
+      call printf
+      ; pop rax
+      mov rbx, [cards_scores_players_current]
+      xor rax, rax
+      mov al, [read_buffer+1]
+      
+      cmp BYTE [rbx+r12], 21
+      jl .nnext
+      cmp BYTE [rbx+r12], 21
+      je .nnext
+      mov rdi, other_lost_msg
+      mov rsi, r12
+      xor rdx, rdx
+      mov dl, BYTE [rbx+r12]
+      push r12
+      push r9
+      call printf
+      pop r9
+      pop r12
+      call calculate_results
+
+
+      ; mov byte [rbx+r12], 0
+
+      .nnext:
+      jmp _read
+
+      .next12:
+      cmp BYTE [read_buffer], '#'
+      jne .next2
+
+      mov rbx, [cards_scores_players_current]
+      mov rdi, other_stop_msg
+      mov rsi, r12
+      xor rdx, rdx
+      mov dl, BYTE [rbx+r12]
+      push r12
+      push r10
+      call printf
+      pop r10
+      pop r12
+        mov r10, [enders]
+  dec BYTE [r10+64]
+
+    mov r10, [enders]
+  cmp BYTE [r10+64], 0
+  jne .ncalc
+  mov rsi, endgame_msg
+  call print_str
+  inc BYTE [r10+64]
+
+
+  call calculate_results
+  ;   mov rsi, endgame_msg
+  ; call print_str 
+  jmp _read
+
+
+  .ncalc:
+  mov rdi, left_players_msg
+  xor rax, rax
+  mov BYTE al, [r10+64]
+  mov rsi, rax
+
+  call printf
+
+      ; mov byte [rbx+r12], 0
+      jmp _read
+
+
+
+
+      .next2:
       mov rdi, from_msg
       mov rsi, r12
       call printf
@@ -206,62 +330,49 @@ jmp _read
 
 _write:
 
-    ; mov rsi, buffer_help
-    ; mov rax, r12
-    ; call number_str
-    ; call print_str
-    ; mov rsi, buffer
-    ; call input_keyboard
-    
-    ;;Отправляем сообщение на клиенту
-  ;   xor rax, rax
-  ; mov rdi, [address]
-  
-  ; cmp BYTE [rdi+1],0
-  ;   jle _write
-        ;;Запираем семафор
-
-
     mov rdi, [ids]
     mov rsi, sm1
     mov rdx,1
     mov rax, 65
     syscall
     
-    ; dec BYTE [rdi+1]
 
-    mov rsi, ping_msg
-    call print_str
-
-    ; xor rax, rax
-    ; mov rdi, [address]
-    ; mov al, [rdi+1]
-    ; mov rsi, buffer
-    ; call number_str
-    ; call print_str
-    ; call new_line
+    mov rdi, [cards_scores_players_current]
+    xor rax, rax
+    mov  BYTE al, [rdi+64]
+    mov rsi, rax
+    mov rdi, ping_msg
+    call printf
 
 
+  ; mov rsi, message_to_all
+  
     mov rax, 1
     mov rdi, r12
-    mov rsi, automated_response
-    mov rdx, 13
+    mov rsi, [message_to_all]
+    mov rdx, 64
     syscall
-
-      ; mov rdi, [address]
-
-
-    
-
-
 
 jmp _write
 
+; _write_once:
+
+
+;     mov rax, 1
+;     mov rdi, r12
+;     mov rsi, once_buf
+;     mov rdx, 13
+;     syscall
+
+; jmp _write_once
       
 _input:
 
-    mov rsi, buffer
+    mov rsi, [message_to_all]
     call input_keyboard
+    mov rsi, [message_to_all]
+    mov byte [rsi+63], 0
+    call print_str
     xor rax, rax
     ; mov rdi, [address]
     ; mov BYTE al, [rdi]
@@ -272,18 +383,29 @@ _input:
     ; mov rsi, sm2
     ; mov rdx,0
     ; mov rax, 65
+    xor rcx, rcx
+    mov rdi, [cards_scores_players_current]
+    mov  BYTE cl, [rdi+64]
+
+    .lp2:
+      push rcx
     ; syscall
      ;;Открываем семафор
-    mov rdi, [ids]
-    mov rsi, sm2
-    mov rdx,1
-    mov rax, 65
-    syscall
-    mov rdi, [ids]
-    mov rsi, sm2
-    mov rdx,1
-    mov rax, 65
-    syscall
+      mov rdi, [ids]
+      mov rsi, sm2
+      mov rdx,1
+      mov rax, 65
+      syscall
+      pop rcx
+      dec rcx
+      cmp rcx, 0
+      jne .lp2
+
+    ; mov rdi, [ids]
+    ; mov rsi, sm2
+    ; mov rdx,1
+    ; mov rax, 65
+    ; syscall
     ; mov al, [new_status]
     ; call number_str
     ; call print_str
@@ -293,3 +415,73 @@ _input:
     ; call print_str
 jmp _input
 
+new_game:
+  mov rsi, [cards_scores_players_current]
+  mov rcx, 129
+  .lp:
+  cmp rcx, 64
+  je .nt
+  mov byte [rsi+rcx],0 
+  .nt:
+
+  loop .lp
+  mov rsi, new_game_msg
+  call print_str
+
+
+  mov r10, [enders]
+  xor rax, rax
+  mov rsi, [cards_scores_players_current]
+  mov BYTE al, [rsi+64]
+  mov BYTE [r10+64], al
+  mov rsi, rax
+  mov rdi, reset_amount_msg
+  call printf
+  ret
+
+calculate_results:
+ 
+  mov r10, [enders]
+  dec BYTE [r10+64]
+  cmp BYTE [r10+64], 0
+  je .con
+  ret
+
+  .con:
+  mov rcx, 63
+  xor rax, rax
+  mov rbx, [cards_scores_players_current]
+  .lp:
+  push rcx
+  cmp byte [rbx+rcx], 0
+  je .next
+  xor rax, rax
+  mov BYTE al, [rbx+rcx]
+
+  ; mov rsi, buffer_help
+  ; mov rax, rcx
+  ; call number_str
+  ; call print_str
+  
+  ; push rsi
+  push rbx
+                    push rax
+                    xor rdx, rdx
+                    mov byte dl, [rbx+rcx]
+                    ; xor rdx,rdx
+                    mov rsi, rcx
+                    mov rdi, score_msg
+                    ; mov rsi, score_msg
+                    call printf
+                    ; ; call print_str
+                    pop rax
+                    pop rbx
+                    ; ; pop rsi
+  .next:
+  pop rcx
+  dec rcx
+  cmp rcx, -1
+  jne .lp
+        call new_game
+  ret
+  
